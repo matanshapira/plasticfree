6315aaeeb93d07745bc17b96b65cdd577dbe4d45
Matan Shapira <matan@screenovate.com>
2020-07-07 22:31:12 +0300

    Updates

---
+++ precache-manifest.e1a24ba0e00389b4682a4703c69488f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338f360699690e0bdddca77f595e2dec",
+    "url": "/plasticfree/index.html"
+  },
+  {
+    "revision": "eedf1479f2d85729fd92",
+    "url": "/plasticfree/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ca9b2b709b23eb856d4",
+    "url": "/plasticfree/static/js/2.d068817a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/plasticfree/static/js/2.d068817a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eedf1479f2d85729fd92",
+    "url": "/plasticfree/static/js/main.71acadb3.chunk.js"
+  },
+  {
+    "revision": "ceb1f04442b019754a69",
+    "url": "/plasticfree/static/js/runtime-main.ade09f6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/plasticfree/static/media/logo.5d5d9eef.svg"
+  }
+]);
