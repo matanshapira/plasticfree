d196e848bcf775f0d5de6025a7fe4dae3d2247fb
Matan Shapira <matan@screenovate.com>
2020-07-09 21:42:11 +0300

    plasticfree

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,31 @@ import { requests, addresses } from './content.js';
 import './App.css';
 import copy from 'copy-to-clipboard';
 
+var mixpanel = require('mixpanel-browser');
+mixpanel.init('99f509e7db2efd670d8fa645fa070b1a');
+
+
+var isMobile = {
+  Android: function() {
+      return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,13 +40,14 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    mixpanel.track('Referral Page Shown');
   }
 
   openTicket() {
     copy(this.state.requestBody);
+    mixpanel.track('Open-Ticket Clicked');
     window.open('https://www5.tel-aviv.gov.il/TlvForms/106plus/');
   }
-
   composeEmailUrl() {
     const targets = addresses.join(';');
     return `mailto:${targets}?subject=${this.state.requestSubject}&body=${this.state.requestBody}`;
@@ -31,7 +57,17 @@ class App extends React.Component {
     const targets = addresses.join(';');
     return `https://mail.google.com/mail/?view=cm&fs=1&to=${targets}&su=${this.state.requestSubject}&body=${this.state.requestBody}`;
   }
-  
+
+  onEmailClicked = () => {
+    mixpanel.track('Compose Email Clicked', { client: 'native' });
+    window.open(this.composeEmailUrl());
+  }
+
+  onGmailClicked = () => {
+    mixpanel.track('Compose Email Clicked', { client: 'gmail' });
+    window.open(this.composeGmailUrl());
+  }
+
 
   render() {
     return (
@@ -47,8 +83,8 @@ class App extends React.Component {
         <div><button onClick={() => this.openTicket()}>פתח פניה במוקד העיריה</button></div>
         <p />
         <div style={{ fontWeight: 'bold'}}>שליחת מייל לאנשי מפתח בעיריה</div>
-        <div><button onClick={() => window.open(this.composeEmailUrl())}>שליחת מייל בעזרת תוכנת המייל שלך</button></div>
-        <div><button onClick={() => window.open(this.composeGmailUrl())}>שליחת מייל בעזרת GMail</button></div>
+        <div><button onClick={this.onEmailClicked}>שליחת מייל בעזרת תוכנת המייל שלך</button></div>
+        <div> {isMobile.any() ? '' : <button onClick={this.onGmailClicked}>שליחת מייל בעזרת GMail</button>}</div>
         <p />
         <div style={{ fontWeight: 'bold'}}>תוכן הפניה:</div>
         <p></p>
